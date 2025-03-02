5cbc937e996f1dd8140150f6ea4233375f3efc98
Helin Varoğlu <helinv@sabanciuniv.edu>
2025-03-02 15:54:10 +0300

    Added header

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -3,7 +3,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <div className="h-screen bg-cover bg-center" style={{ backgroundImage: "url('/background.png')" }}>
-      <h1 className="text-white text-5xl">E-Kartela</h1>
+      <h1 className={styles.mainHeader}>Kartex</h1>
     </div>
   );
 }
