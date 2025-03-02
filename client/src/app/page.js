07ec27031bc075909a9033ddf885d3cb6e2f8149
Helin Varoğlu <helinv@sabanciuniv.edu>
2025-03-02 14:58:03 +0300

    Initialized setup

---
+++ client/src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-cover bg-center" style={{ backgroundImage: "url('/background.png')" }}>
+      <h1 className="text-white text-3xl">E-Kartela</h1>
+      <button>Hi</button>
+    </div>
+  );
+}
