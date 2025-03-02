919689743452950899061e8947895017d709d54c
Helin Varoğlu <helinv@sabanciuniv.edu>
2025-03-02 16:53:35 +0300

    Added submit button

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -4,6 +4,20 @@ export default function Home() {
   return (
     <div className="h-screen bg-cover bg-center" style={{ backgroundImage: "url('/background.png')" }}>
       <h1 className={styles.mainHeader}>Kartex</h1>
+
+      <div className={ styles.inputGroup }>
+        <div className="input-group mb-3">
+          <input type="text" className={`form-control ${styles.formControl}`} placeholder="Kullanıcı Adı" aria-label="Username"/>
+        </div>
+
+        <div className="input-group mb-3">
+          <input type="text" className={`form-control ${styles.formControl}`} placeholder="Şifre" aria-label="Password" />
+        </div>
+
+        <div>
+          <button className={`btn ${styles.submitButton}`} type="submit">Giriş</button>
+        </div>
+      </div>
     </div>
   );
 }
