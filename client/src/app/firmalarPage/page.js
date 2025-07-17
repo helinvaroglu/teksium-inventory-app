3920777bac9c41afcc2eaba49178dcac65a83c75
Helin Varoğlu <helinv@sabanciuniv.edu>
2025-07-17 15:55:38 +0300

    Added gönder page

---
+++ client/src/app/firmalarPage/page.js
@@ -0,0 +1,15 @@
+"use client";
+import styles from './page.module.css';
+import { useRouter, usePathname } from 'next/navigation';
+import Navbar from "../../components/navbar/Navbar";
+import Searchbar from '../../components/searchbar/Searchbar';
+
+export default function DesenlerPage() {
+    return (
+        <div className={`background ${styles.background}`}>
+            <Navbar />
+            <Searchbar />
+
+        </div>
+    );
+}
