c43b44cf1caf0a78f0a2bb53fe97b516561fad79
Helin Varoğlu <helinv@sabanciuniv.edu>
2025-07-17 17:27:36 +0300

    Added mock data to desenler page

---
+++ client/src/app/api/desenler/route.js
@@ -0,0 +1,63 @@
+export async function GET() {
+    const desenlerData = [
+        {
+            id: 1,
+            name: "Desen 1",
+            code: "D001",
+            color: "Red",
+            stock: 12,
+            price: "$5"
+        },
+        {
+            id: 2,
+            name: "Desen 2",
+            code: "D002",
+            color: "Blue",
+            stock: 5,
+            price: "$4"
+        },
+        {
+            id: 3,
+            name: "Desen 3",
+            code: "D003",
+            color: "Green",
+            stock: 20,
+            price: "$2"
+        },
+        {
+            id: 4,
+            name: "Desen 4",
+            code: "D004",
+            color: "Purple",
+            stock: 15,
+            price: "$4"
+        },
+        {
+            id: 5,
+            name: "Desen 5",
+            code: "D005",
+            color: "Yellow",
+            stock: 2,
+            price: "$2"
+        },
+        {
+            id: 6,
+            name: "Desen 6",
+            code: "D006",
+            color: "Blue",
+            stock: 9,
+            price: "$4"
+        },
+        {
+            id: 7,
+            name: "Desen 7",
+            code: "D007",
+            color: "White",
+            stock: 22,
+            price: "$1"
+        },
+
+    ];
+
+    return Response.json(desenlerData);
+}
--- client/src/app/desenlerPage/page.js
+++ client/src/app/desenlerPage/page.js
@@ -1,14 +1,46 @@
 "use client";
+import { useEffect, useState } from 'react';
 import styles from './page.module.css';
 import { useRouter, usePathname } from 'next/navigation';
 import Navbar from "../../components/navbar/Navbar";
 import Searchbar from '../../components/searchbar/Searchbar';
 
 export default function DesenlerPage() {
+
+    const [desenler, setDesenler] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('/api/desenler') // TODO: Change with real API
+            .then(res => res.json())
+            .then(data => {
+                setDesenler(data);
+                setLoading(false);
+            })
+            .catch(err => {
+                console.error("Failed to fetch:", err);
+                setLoading(false);
+            });
+    }, []);
+
     return (
         <div className={`background ${styles.background}`}>
             <Navbar />
             <Searchbar />
+
+            {loading ? (
+                <p>Yükleniyor...</p>
+            ) : (
+                <div >
+                    {desenler.map(desen => (
+                        <div key={desen.id} className={styles.productframe}>
+                            <h2 className={styles.productheader}>{desen.name}</h2>
+                            <p className={styles.productcode}>Kod: {desen.code}</p>
+                            <p className={styles.productprice}>{desen.price}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
 
         </div>
     );
